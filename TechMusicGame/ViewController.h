82a29b372f01779fea4757a24dc0d09dc92dba0e
Rey <quit9909@icloud.com>
2015-03-10 23:40:13 +0900

    Initial Commit

---
+++ TechMusicGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TechMusicGame
+//
+//  Created by ReyMasuda on 2015/03/10.
+//  Copyright (c) 2015年 ReyMasuda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
